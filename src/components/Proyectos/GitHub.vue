512ff0ba5a03a5b68252fca095687c4421596f11
Carlos Ernesto Díaz Basante <carlodiaz@umariana.edu.co>
2021-11-15 20:45:14 -0500

    Creado componente con la logica para aceptar los links de los repositorios

---
+++ src/components/Proyectos/GitHub.vue
@@ -0,0 +1,28 @@
+<template>
+  <v-card :color="color">
+    <v-card-text>
+      <v-form>
+        <v-btn text :disabled="private">
+          {{ !private ? "Ver repositorio" : "Este repositorio es privado" }}
+        </v-btn>
+      </v-form>
+    </v-card-text>
+  </v-card>
+</template>
+
+<script lang="ts">
+import { mapState } from "vuex";
+
+export default {
+  name: "GitHub",
+  computed: {
+    ...mapState(["color"]),
+  },
+  props: {
+    link: String,
+    private: Boolean,
+  },
+};
+</script>
+
+<style scoped></style>
