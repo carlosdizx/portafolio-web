a936913e073d5cf4d56ee3c06d564b3c758b80f6
Carlos Ernesto Díaz Basante <carlodiaz@umariana.edu.co>
2021-11-15 20:57:38 -0500

    Creado un componente que contendra la logica de los proyectos

---
+++ src/components/Proyecto.vue
@@ -0,0 +1,33 @@
+<template>
+  <v-card :color="color">
+    <v-card-text>
+      <v-form>
+        <h1>{{ titulo }}</h1>
+        <h3>{{ subtitulo }}</h3>
+        <v-alert>{{ descripcion }}</v-alert>
+      </v-form>
+    </v-card-text>
+  </v-card>
+</template>
+
+<script lang="ts">
+import { mapState } from "vuex";
+
+export default {
+  name: "Proyecto",
+  computed: {
+    ...mapState(["color"]),
+  },
+  props: {
+    titulo: String,
+    subtitulo: String,
+    descripcion: String,
+    imagen: String,
+    link: String,
+    page: String,
+    privado: Boolean,
+  },
+};
+</script>
+
+<style scoped></style>
