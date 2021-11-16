59c46e44f603dc0c5a46eda85db00218730d5fdb
Carlos Ernesto Díaz Basante <carlodiaz@umariana.edu.co>
2021-11-16 00:10:59 -0500

    Mejorado el formulario

--- src/components/FormProyecto.vue
+++ src/components/FormProyecto.vue
@@ -3,8 +3,22 @@
     <v-card-title>Formulario de registro de proyectos</v-card-title>
     <v-card-text>
       <v-form>
-        <v-text-field label="XD" />
+        <v-text-field label="Titulo" />
+        <v-combobox
+          outlined
+          dense
+          multiple
+          hint="En formato mdi"
+          label="Tecnologias"
+          small-chips
+        ></v-combobox>
+        <v-textarea label="Descripcion" />
+        <v-text-field label="Pagina" />
+        <v-text-field label="Repositorio" />
       </v-form>
+      <v-card-text>
+        <v-btn color="success">Registrar</v-btn>
+      </v-card-text>
     </v-card-text>
   </v-card>
 </template>
