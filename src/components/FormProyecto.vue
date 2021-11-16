1a60da9fb2289e6a21fd9b49117b0d77b191e1db
Carlos Ernesto Díaz Basante <carlodiaz@umariana.edu.co>
2021-11-16 13:15:29 -0500

    Se implmento el metodo de registrar proyectos

--- src/components/FormProyecto.vue
+++ src/components/FormProyecto.vue
@@ -3,29 +3,101 @@
     <v-card-text><h1>Formulario de registro de proyectos</h1></v-card-text>
     <v-card-text>
       <v-form>
-        <v-text-field outlined dense label="Titulo" />
+        <v-text-field
+          v-model="titulo"
+          :color="color"
+          outlined
+          dense
+          label="Titulo"
+        />
         <v-combobox
+          v-model="tecnologias"
           outlined
           dense
           multiple
           hint="En formato mdi"
           label="Tecnologias"
           small-chips
-        ></v-combobox>
-        <v-textarea outlined dense label="Descripcion" />
-        <v-text-field outlined dense label="Pagina" />
-        <v-text-field outlined dense label="Repositorio" />
+          :color="color"
+        />
+        <v-textarea
+          v-model="descripcion"
+          :color="color"
+          outlined
+          dense
+          label="Descripcion"
+        />
+        <v-text-field
+          v-model="pagina"
+          :color="color"
+          outlined
+          dense
+          label="Pagina"
+          type="url"
+        />
+        <v-switch
+          v-model="privado"
+          :color="color"
+          label="Este repositorio es privado"
+        />
+        <v-text-field
+          v-model="repositorio"
+          :color="color"
+          outlined
+          dense
+          label="Repositorio"
+          type="url"
+        />
+        <div class="text-center">
+          <v-btn
+            @click="registrarProyecto"
+            class="white--text"
+            :color="color"
+            block
+          >
+            Registrar
+          </v-btn>
+        </div>
       </v-form>
-      <v-card-text>
-        <v-btn color="success">Registrar</v-btn>
-      </v-card-text>
     </v-card-text>
   </v-card>
 </template>
 
 <script>
+import { mapState } from "vuex";
+import { REGISTRAR_PROYECTO } from "@/services/recursos/proyectos";
 export default {
   name: "FormProyecto",
+  data: () => ({
+    titulo: "",
+    tecnologias: [],
+    descripcion: "",
+    pagina: "",
+    repositorio: "",
+    privado: true,
+  }),
+  computed: {
+    ...mapState(["color"]),
+  },
+  methods: {
+    async registrarProyecto() {
+      const datos = {
+        titulo: this.titulo,
+        tecnologias: this.tecnologias,
+        descripcion: this.descripcion,
+        pagina: this.pagina,
+        repositorio: this.repositorio,
+        privado: this.privado,
+      };
+      await REGISTRAR_PROYECTO(datos);
+      this.titulo = "";
+      this.tecnologias = [];
+      this.descripcion = "";
+      this.pagina = "";
+      this.repositorio = "";
+      this.privado = true;
+    },
+  },
 };
 </script>
 
--- src/components/ListadoProyectos.vue
+++ src/components/ListadoProyectos.vue
@@ -1,6 +1,8 @@
 <template>
   <v-card>
-    <v-card-text><h1>Proyectos desarrollados</h1></v-card-text>
+    <v-card-text v-if="proyectos.length > 0">
+      <h1>Proyectos desarrollados</h1>
+    </v-card-text>
     <Proyecto
       class="my-5"
       v-for="(proyecto, index) of proyectos"
@@ -17,24 +19,6 @@ export default {
   name: "ListadoProyectos",
   data: () => ({
     proyectos: [],
-    datos: {
-      titulo: "Proyecto lorem ipsum",
-      tecnologias: ["mdi-angular", "mdi-language-javascript"],
-      descripcion:
-        "Lorem Ipsum is simply dummy text of the printing and typesetting" +
-        " industry. Lorem Ipsum has been the industry's standard" +
-        " dummy text ever since the 1500s, when an unknown printer" +
-        " took a galley of type and scrambled it to make a type" +
-        " specimen book. It has survived not only five centuries," +
-        " but also the leap into electronic typesetting, remaining" +
-        " essentially unchanged. It was popularised in the 1960s with" +
-        " the release of Letraset sheets containing Lorem Ipsum passages," +
-        " and more recently with desktop publishing software like" +
-        " Aldus PageMaker including versions of Lorem Ipsum.",
-      pagina: "https://portafolio-carlos-diaz.netlify.app/",
-      repositorio: "https://portafolio-carlos-diaz.netlify.app/",
-      privado: true,
-    },
   }),
   components: { Proyecto },
   methods: {
